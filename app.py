272c73d965c95ffce6e5328c8b9bf9d600b1aa87
Ethan Guo <ethguo@gmail.com>
2016-02-20 22:06:48 -0500

    Basic API call/response

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from alchemyapi import AlchemyAPI
+from flask import Flask, request
+import jinja2
+
+alchemyapi = AlchemyAPI()
+
+jinja_env = jinja2.Environment(
+		loader=jinja2.FileSystemLoader("templates")
+		)
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def get_index():
+	template = jinja_env.get_template("index.html")
+	return template.render()
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/response", methods=["POST"])
+def get_response():
+	doc = str(request.form["doc"])
+	alchemy = alchemyapi.sentiment("text", doc)
+	try:
+		res = alchemy["docSentiment"]
+	except KeyError:
+		res = alchemy
+	return str(res)
+
+app.run(debug=True)
