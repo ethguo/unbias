78e155357b27aa41c4365f15de0a181e0b727e7d
Jacob Sawatzky <Jacob Sawatzky>
2016-02-21 21:04:47 -0500

    Updated annotation gen

--- app.py
+++ app.py
@@ -18,8 +18,17 @@ CONCEPT_SENTIMENT_THRESHOLD = 0.25
 
 alchemyapi = AlchemyAPI()
 
-def annotate(s):
-	a = "This sentence has problems and stuff."
+def annotate(s, entities, keywords, concepts):
+	a = []
+
+	for dict in [entities, keywords, concepts]:
+		for key, value in dict.iteritems():
+
+			if s.find(key) > -1:
+				a.append("Possible " + value " bias towards " + key)
+
+	if len(a) == 0: a = None
+
 	return (s, a)
 
 app = Flask(__name__)
@@ -53,10 +62,16 @@ def get_result():
 		topicEntities = filter(lambda e: float(e["relevance"]) > TOPIC_ENTITIES_THRESHOLD, entities)
 		topicKeywords = filter(lambda k: float(k["relevance"]) > TOPIC_KEYWORDS_THRESHOLD, keywords)
 		topicConcepts = filter(lambda c: float(c["relevance"]) > TOPIC_CONCEPTS_THRESHOLD, concepts)
+		biasedEntitiesList = filter(lambda e: float(e["sentiment"]["score"]) > ENTITY_SENTIMENT_THRESHOLD, entities)
+		biasedEntities = {biasedEntitiesList[i]["text"]: biasedEntitiesList[i]["sentiment"]["type"] for i in range(len(biasedEntitiesList))}
+		biasedKeywordsList = filter(lambda e: float(e["sentiment"]["score"]) > ENTITY_SENTIMENT_THRESHOLD, entities)
+		biasedKeywords = {biasedKeywordsList[i]["text"]: biasedKeywordsList[i]["sentiment"]["type"] for i in range(len(biasedKeywordsList))}
+		biasedConceptsList = filter(lambda e: float(e["sentiment"]["score"]) > ENTITY_SENTIMENT_THRESHOLD, entities)
+		biasedConcepts = {biasedConceptsList[i]["text"]: biasedConceptsList[i]["sentiment"]["type"] for i in range(len(biasedConceptsList))}
 
 		docSentences = tokenize.sent_tokenize(doc)
 
-		sentencesAnnotated = [ annotate(s) for s in docSentences]
+		sentencesAnnotated = [ annotate(s, biasedEntities, biasedKeywords, biasedConcepts) for s in docSentences]
 
 		template = jinja_env.get_template("result.html")
 
